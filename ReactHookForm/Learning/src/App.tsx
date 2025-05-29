fcade38335fa66cfa0ab2b807e651e175078bb91
Shahid Khan <shahidseran786@gmail.com>
2025-05-29 16:35:24 +0530

    Basic Setup for Any Proejct

--- ReactHookForm/Learning/src/App.tsx
+++ ReactHookForm/Learning/src/App.tsx
@@ -1,6 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-black text-white ">
+      <BrowserRouter>
+        <Routes>
+           <Route path="/" element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 export default App
