16ec0a3d50361da2a14fceed7a0745e020832380
Shahid Khan <shahidseran786@gmail.com>
2025-05-29 17:35:18 +0530

    Nested Routes

--- ReactRouterDOM/Learning/src/App.tsx
+++ ReactRouterDOM/Learning/src/App.tsx
@@ -1,14 +1,38 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom"
 import Home from "./components/Home"
+import Dashboard from "./components/Dashboard"
+import Profile from "./components/Dashboard/Profile";
+import Help from "./components/Dashboard/Help";
+import About from "./components/Dashboard/About";
+
+const DashbaordWelecome=()=>{
+  return (<>
+    <h1>
+      Welcome Shahid, On Dashbaord Page
+    </h1>
+  </>)
+}
+
 const App = () => {
   return (
     <div className="bg-black text-white ">
       <BrowserRouter>
-        <Routes>
-           <Route path="/" element={<Home/>}/>
-        </Routes>
+        <nav className="flex p-4 gap-2">
+          <NavLink to={"/dashbaord"}>Dashbaord</NavLink>
+        </nav>
+        <div className="h-[calc(100vh-57px)]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashbaord" element={<Dashboard />}>
+              <Route index element={<DashbaordWelecome />} />
+              <Route path="/dashbaord/help" element={<Help />} />
+              <Route path="/dashbaord/about" element={<About />} />
+              <Route path="/dashbaord/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 export default App
--- ReactRouterDOM/Learning/src/components/Home.tsx
+++ ReactRouterDOM/Learning/src/components/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="flex justify-center items-center font-bold text-4xl h-screen ">Learning , React Hook Form </div>
+    <div className="flex justify-center items-center font-bold text-4xl h-screen ">Learning , React Router DOM </div>
   )
 }
 export default Home
